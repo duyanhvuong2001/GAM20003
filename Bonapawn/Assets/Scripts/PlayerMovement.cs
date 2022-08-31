2d5702d8136a8a3f9c657203a9b1dff94714cf68
duyanhvuong2001 <102603197@student.swin.edu.au>
2022-08-31 15:21:33 +1000

    first commit

---
+++ Bonapawn/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    private bool walking;
+
+    private Rigidbody2D rb;
+    // private Animator anim;
+
+    private Vector2 movement;
+    private Vector3 moveToPosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(!walking)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+
+            if(movement.x != 0)
+            {
+                movement.y = 0;
+            }
+            if(movement != Vector2.zero)
+            {
+
+                moveToPosition = transform.position + new Vector3(movement.x, movement.y, 0);
+                // anim.SetFloat("X", movement.x);
+                // anim.SetFloat("Y", movement.y);
+                // anim.SetBool("walking", true)
+
+                StartCoroutine(Move(moveToPosition));
+            }
+
+            // anim.SetBool("walking", walking);   
+        }
+    }
+
+    IEnumerator Move(Vector3 newPos)
+    {
+        walking = true;
+
+        while((newPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPos, moveSpeed * Time.fixedDeltaTime);
+            yield return null;
+        }
+        transform.position = newPos;
+
+        walking = false;
+
+    }
+
+
+}
