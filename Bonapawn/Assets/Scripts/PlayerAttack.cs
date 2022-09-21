7aa8686d12e9e705f7689c9e1aff77f0aa6b90a2
Cody <103610020@student.swin.edu.au>
2022-09-21 15:18:53 +1000

    Basic attack started, still needs more work

--- Bonapawn/Assets/Scripts/PlayerAttack.cs
+++ Bonapawn/Assets/Scripts/PlayerAttack.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public float attackDuration = 20f;
+    public float attackDelay = 10f;
+    public float attackDurationActive = 0f;
+    public float attackDelayActive = 0f;
+
+    public GameObject atkObj1;
+    public GameObject atkObj2;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        atkObj1.SetActive(false);
+        atkObj2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.eulerAngles = new Vector3(
                 transform.eulerAngles.x,
                 transform.eulerAngles.y,
-               180
+               //180
+               0
            );
         }
+        
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.eulerAngles = new Vector3(
@@ -29,6 +42,8 @@ public class PlayerAttack : MonoBehaviour
                 0
             );
         }
+        
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.eulerAngles = new Vector3(
@@ -37,6 +52,7 @@ public class PlayerAttack : MonoBehaviour
                 90
             );
         }
+        
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             transform.eulerAngles = new Vector3(
@@ -44,6 +60,32 @@ public class PlayerAttack : MonoBehaviour
                  transform.eulerAngles.y,
                 270
             );
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (attackDelayActive <= 0f)
+            {
+                atkObj1.SetActive(true);
+                atkObj2.SetActive(true);
+                attackDurationActive = attackDuration;
+            }
+        }
+
+        if(attackDurationActive > 0f)
+        {
+            attackDurationActive -= 1f;
+            if (attackDurationActive <= 0f)
+            {
+                attackDelayActive = attackDelay;
+                atkObj1.SetActive(false);
+                atkObj2.SetActive(false);
+            }
+        }
+
+        if(attackDelayActive > 0f)
+        {
+            attackDelayActive -= 1f;
         }
     }
 }
