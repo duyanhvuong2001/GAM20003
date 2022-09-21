30edd4e1dcee4d89a4090df2ebba094e17373619
duyanhvuong2001 <102603197@student.swin.edu.au>
2022-09-21 15:40:04 +1000

    Bishop added

--- Bonapawn/Assets/Scripts/ChessBehaviour/DiagonalMoveBehaviour.cs
+++ Bonapawn/Assets/Scripts/ChessBehaviour/DiagonalMoveBehaviour.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class DiagonalMoveBehaviour : ChessBehaviour
 {
+    private int _limit;
+
+    public DiagonalMoveBehaviour(int limit)
+    {
+        _limit = limit;
+    }
+
     public override List<Path> ExploreAvailablePaths(Vector3 currentPosition)
     {
-        throw new System.NotImplementedException();
+        List<Path> paths = new List<Path>();
+        //Limit the range of available moves
+        for(int i=-_limit;i<=_limit;i++)
+        {
+            if(i!=0)
+            {
+                //A path with the same x,y relatively to the current pos
+                Path upperPath = new Path(currentPosition + new Vector3(i, i, 0));
+
+                //Another flipped path
+                Path lowerPath = new Path(currentPosition + new Vector3(i, -i, 0));
+
+                //add them to the list of paths
+                paths.Add(upperPath);
+                paths.Add(lowerPath);
+            }
+        }
+        return paths;
     }
 }
---
+++ Bonapawn/Assets/Scripts/ChessPieces/Bishop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.ChessPieces
+{
+    public class Bishop : ChessPiece
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            behaviours.Add(new DiagonalMoveBehaviour(10));
+            moveCooldown = 1.5f;
+        }
+    }
+}
