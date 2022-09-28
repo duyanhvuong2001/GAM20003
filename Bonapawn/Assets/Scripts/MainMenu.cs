b28dfafa92218435e7144947ed1767d4007640ca
RetroAstronaut <103610732@student.swin.edu.au>
2022-09-28 20:08:11 +1000

    Menu 28/09/2022

--- Bonapawn/Assets/Scripts/MainMenu.cs
+++ Bonapawn/Assets/Scripts/MainMenu.cs
@@ -8,4 +8,10 @@ public class MainMenu : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
