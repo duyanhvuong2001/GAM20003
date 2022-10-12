d452da6c4d01e8fbf89ae7d8f8a0d94fc525be47
duyanhvuong2001 <102603197@student.swin.edu.au>
2022-10-12 15:04:11 +1100

    Update ChessBehaviour.cs

--- Bonapawn/Assets/Scripts/ChessBehaviour/ChessBehaviour.cs
+++ Bonapawn/Assets/Scripts/ChessBehaviour/ChessBehaviour.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public abstract class ChessBehaviour
 {
-    public abstract List<Path> ExploreAvailablePaths(Vector3 currentPosition);
+    private RaycastHit2D hit;
+
+    public abstract List<Path> ExploreAvailablePaths(Vector3 currentPosition, BoxCollider2D collider);
+
+    public bool PathAvailable(Vector3 currentPosition, Vector3 destination, BoxCollider2D collider)
+    {
+        //Cast a box at the destination
+        hit = Physics2D.BoxCast(
+            currentPosition,
+            collider.size,
+            0,
+            destination - currentPosition,
+            Mathf
+            );
+    }
 }
